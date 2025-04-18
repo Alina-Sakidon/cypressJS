ad76dce81d588c0379e72b40e8bfcda00622a1d0
asakidon <alinasakidon@gmail.com>
2025-04-18 15:34:17 +0300

    Add cypress config file

---
+++ cypress.config.js
@@ -0,0 +1,23 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: 'https://prozorro.gov.ua/en/',
+    setupNodeEvents(on, config) {
+      // Set up event listeners if necessary
+    },
+    viewportWidth: 1280,
+    viewportHeight: 720,
+    defaultCommandTimeout: 10000,
+    pageLoadTimeout: 60000,
+    video: false,
+    retries: {
+      runMode: 2,
+      openMode: 0,
+    },
+    env: {
+      username: 'testuser',
+      password: 'testpassword',
+    },
+  },
+});
